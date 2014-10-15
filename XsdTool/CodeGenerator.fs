b94ee8e4e0d874e7b80b871c2d26513f26153c4f
Janno Põldma <janno.poldma@gmail.com>
2014-10-15 15:46:13 +0300

    Refactor code.

---
+++ XsdTool/CodeGenerator.fs
@@ -0,0 +1,25 @@
+﻿module XsdTool.CodeGenerator
+
+open System.CodeDom
+open System.Collections.Generic
+open System.IO
+open System.Xml
+open System.Xml.Schema
+
+let settings = XmlReaderSettings()
+settings.ValidationEventHandler.Add(fun e -> eprintfn "%s" e.Message)
+
+let openSchema schemaFile =
+    use reader = XmlReader.Create(File.OpenRead(schemaFile), settings)
+    XmlSchema.Read(reader, fun _ e -> eprintfn "%s" e.Message)
+
+let getRequestResponse (schema: XmlSchema) =
+    let elements = [for e in schema.Items -> e]
+                   |> Seq.choose (fun e -> match e with | :? XmlSchemaElement as e -> Some e | _ -> None)
+                   |> Seq.fold (fun (acc: Dictionary<string, XmlSchemaElement>) e -> acc.Add(e.Name, e); acc) (Dictionary<_, _>())
+    (elements.["request"], elements.["response"])
+
+let BuildCodeUnit schemaFile =
+    let schema = openSchema schemaFile
+    let request, response = getRequestResponse schema
+    CodeCompileUnit()
--- XsdTool/Program.fs
+++ XsdTool/Program.fs
@@ -2,41 +2,16 @@
 
 open FSharp.Configuration
 open Microsoft.CSharp
-open System.CodeDom
 open System.CodeDom.Compiler
-open System.Collections.Generic
-open System.Diagnostics
 open System.IO
-open System.Xml
-open System.Xml.Schema
+open XsdTool.CodeGenerator
 
 type Settings = AppSettings<"App.config">
 
-module Generate =
-    let settings = XmlReaderSettings()
-    settings.ValidationEventHandler.Add(fun e -> eprintfn "%s" e.Message)
-
-    let openSchema schemaFile =
-        use reader = XmlReader.Create(File.OpenRead(schemaFile), settings)
-        XmlSchema.Read(reader, fun _ e -> eprintfn "%s" e.Message)
-
-    let getRequestResponse (schema: XmlSchema) =
-        let elements = [for e in schema.Items -> e]
-                       |> Seq.choose (fun e -> match e with | :? XmlSchemaElement as e -> Some e | _ -> None)
-                       |> Seq.fold (fun (acc: Dictionary<string, XmlSchemaElement>) e -> acc.Add(e.Name, e); acc) (Dictionary<_, _>())
-        (elements.["request"], elements.["response"])
-
-    let BuildCodeUnit schemaFile =
-        let schema = openSchema schemaFile
-        CodeCompileUnit()
-
 [<EntryPoint>]
 let main argv =
-    let path = Path.GetFullPath(match Settings.XsdSearchPath with
-                                | null | "" -> "."
-                                | path -> path)
-
-    let codeUnits = Directory.GetFiles(path, "*.xsd") |> Array.map (Generate.BuildCodeUnit)
+    let path = Path.GetFullPath(match Settings.XsdSearchPath with | null | "" -> "." | path -> path)
+    let codeUnits = Directory.GetFiles(path, "*.xsd") |> Array.map (BuildCodeUnit)
 
     use codeProvider = new CSharpCodeProvider()
     let parameters = CompilerParameters(GenerateExecutable=false, OutputAssembly=Settings.AssemblyName)
