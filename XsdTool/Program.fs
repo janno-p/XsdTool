a78f76fee329b5c8ceae9458820d647f2b0a128b
Janno Põldma <janno.poldma@gmail.com>
2014-10-15 15:39:21 +0300

    Using App.config to customize output assembly.

--- XsdTool/Program.fs
+++ XsdTool/Program.fs
@@ -1,6 +1,8 @@
 ﻿module XsdTool.Program
 
+open FSharp.Configuration
 open Microsoft.CSharp
+open System.CodeDom
 open System.CodeDom.Compiler
 open System.Collections.Generic
 open System.Diagnostics
@@ -8,34 +10,37 @@ open System.IO
 open System.Xml
 open System.Xml.Schema
 
+type Settings = AppSettings<"App.config">
+
 module Generate =
     let settings = XmlReaderSettings()
-    settings.ValidationEventHandler.Add(fun e -> printfn "%s" e.Message)
+    settings.ValidationEventHandler.Add(fun e -> eprintfn "%s" e.Message)
 
-    let reader = XmlReader.Create(File.OpenRead("AnnaIsikuKvalifikatsioonid.xsd"), settings)
+    let openSchema schemaFile =
+        use reader = XmlReader.Create(File.OpenRead(schemaFile), settings)
+        XmlSchema.Read(reader, fun _ e -> eprintfn "%s" e.Message)
 
-    let doc = XmlSchema.Read(reader, fun _ e -> printfn "%s" e.Message)
-    printfn "%s" doc.TargetNamespace
+    let getRequestResponse (schema: XmlSchema) =
+        let elements = [for e in schema.Items -> e]
+                       |> Seq.choose (fun e -> match e with | :? XmlSchemaElement as e -> Some e | _ -> None)
+                       |> Seq.fold (fun (acc: Dictionary<string, XmlSchemaElement>) e -> acc.Add(e.Name, e); acc) (Dictionary<_, _>())
+        (elements.["request"], elements.["response"])
 
-    let elements = [for e in doc.Items -> e]
-                   |> Seq.choose (fun e -> match e with
-                                           | :? XmlSchemaElement as e -> Some e
-                                           | _ -> None)
-                   |> Seq.fold (fun (acc : Dictionary<string, XmlSchemaElement>) e ->
-                                    acc.Add(e.Name, e)
-                                    acc) (Dictionary<_, _>())
-
-    let qn name = XmlQualifiedName(name, doc.TargetNamespace)
-    let request = elements.["request"]
-    let response = elements.["response"]
+    let BuildCodeUnit schemaFile =
+        let schema = openSchema schemaFile
+        CodeCompileUnit()
 
 [<EntryPoint>]
 let main argv =
-    printfn "%O" Generate.request
-    printfn "%O" Generate.response
+    let path = Path.GetFullPath(match Settings.XsdSearchPath with
+                                | null | "" -> "."
+                                | path -> path)
+
+    let codeUnits = Directory.GetFiles(path, "*.xsd") |> Array.map (Generate.BuildCodeUnit)
 
     use codeProvider = new CSharpCodeProvider()
-    let parameters = CompilerParameters(GenerateExecutable=false, OutputAssembly="AutoGen.dll")
-    let results = codeProvider.CompileAssemblyFromSource(parameters, "")
-    printfn "%A" ([for str in results.Output -> str] |> Seq.toArray)
+    let parameters = CompilerParameters(GenerateExecutable=false, OutputAssembly=Settings.AssemblyName)
+    let results = codeProvider.CompileAssemblyFromDom(parameters, codeUnits)
+
+    [for str in results.Output -> str] |> Seq.iter (printfn "%s")
     0
