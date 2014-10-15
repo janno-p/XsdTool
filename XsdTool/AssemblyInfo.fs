86f7296d198cbb0c18cc96c8cc1a13ac8024b12c
Janno Põldma <janno.poldma@gmail.com>
2014-10-15 14:01:32 +0300

    Initial application.

---
+++ XsdTool/AssemblyInfo.fs
@@ -0,0 +1,15 @@
+﻿module XsdTool.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<AssemblyTitle("XsdTool")>]
+[<AssemblyDescription("")>]
+[<AssemblyConfiguration("")>]
+[<AssemblyCompany("")>]
+[<AssemblyProduct("")>]
+[<AssemblyCopyright("janno")>]
+[<AssemblyTrademark("")>]
+[<AssemblyVersion("1.0.0.0")>]
+
+()
---
+++ XsdTool/Program.fs
@@ -0,0 +1,13 @@
+﻿module XsdTool.Program
+
+open System.CodeDom.Compiler
+open System.Diagnostics
+open Microsoft.CSharp
+
+[<EntryPoint>]
+let main argv =
+    use codeProvider = new CSharpCodeProvider()
+    let parameters = CompilerParameters(GenerateExecutable=false, OutputAssembly="AutoGen.dll")
+    let results = codeProvider.CompileAssemblyFromSource(parameters, "")
+    printfn "%A" ([for str in results.Output -> str] |> Seq.toArray)
+    0
