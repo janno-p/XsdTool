9d76da4b004361525381cbc82871e941d7fb15cd
Janno Põldma <janno.poldma@gmail.com>
2014-10-16 15:53:20 +0300

    Generating types for choice elements. Deserialization method implementation.

--- XsdTool/CodeGenerator.fs
+++ XsdTool/CodeGenerator.fs
@@ -27,7 +27,23 @@ let getRequestResponse (schema: XmlSchema) =
                    |> Seq.fold (fun (acc: Dictionary<string, XmlSchemaElement>) e -> acc.Add(e.Name, e); acc) (Dictionary<_, _>())
     (elements.["request"], elements.["response"])
 
+type ElementType =
+    | SimpleType of System.Type
+    | ComplexType of string
+
+let qn ns n = XmlQualifiedName(n, ns)
+
+let (|XmlSchema|_|) (n: XmlQualifiedName): string option =
+    if n.Namespace = "http://www.w3.org/2001/XMLSchema" then
+        Some n.Name
+    else None
+
+let matchType : XmlQualifiedName -> ElementType = function
+    | XmlSchema "string" -> SimpleType typeof<string>
+    | qn -> failwith <| sprintf "Unable to match type %O!" qn
+
 let createDeserializationMethod (request: XmlSchemaElement) (schema: XmlSchema) =
+    let variableIndex = ref 1
     let requestType = schema |> findComplexType request.SchemaTypeName
 
     let deserializeXmlReaderParameter = CodeParameterDeclarationExpression(typeof<XmlReader>, "reader")
@@ -35,39 +51,104 @@ let createDeserializationMethod (request: XmlSchemaElement) (schema: XmlSchema) 
     let deserializeMethod = CodeMemberMethod(Name="Deserialize")
     deserializeMethod.Parameters.Add(deserializeXmlReaderParameter) |> ignore
     deserializeMethod.ReturnType <- CodeTypeReference(typeof<obj[]>)
-    deserializeMethod.Statements.Add(CodeMethodReturnStatement(CodePrimitiveExpression(null))) |> ignore
     deserializeMethod.Attributes <- MemberAttributes.Public ||| MemberAttributes.Static
 
     let newMembers = List<CodeTypeMember>()
     newMembers.Add(deserializeMethod)
 
+    let varReader = CodeVariableReferenceExpression("reader")
+
     let createChoiceType (choiceElement : XmlSchemaChoice) =
-        let choiceTypeName = match choiceElement.Id with
-                             | null | "" -> failwith "Choice type should have `id` declared"
-                             | id -> id
+        let choiceTypeName = match choiceElement.Id with | null | "" -> failwith "Choice type should have `id` declared" | id -> id
         let choiceType = CodeTypeDeclaration(choiceTypeName, IsClass=true)
-        let tagEnum = CodeTypeDeclaration("Tag", IsEnum=true)
+
+        let tagEnum = CodeTypeDeclaration("Tag", IsEnum=true, TypeAttributes=TypeAttributes.NestedPrivate)
         choiceType.Members.Add(tagEnum) |> ignore
-        for item in choiceElement.Items do
+        choiceType.Members.Add(CodeMemberField(CodeTypeReference("Tag"), "tag", Attributes=MemberAttributes.Private)) |> ignore
+
+        let addedMembers = List<CodeTypeMember>()
+        addedMembers.Add(choiceType)
+
+        let choiceTypeItems = [ for item in choiceElement.Items -> item ]
+        let fieldTypes = choiceTypeItems
+                         |> List.fold (fun (acc: HashSet<_>) item ->
+                                match item with
+                                | :? XmlSchemaElement as element -> acc.Add(matchType element.SchemaTypeName) |> ignore
+                                | :? XmlSchemaSequence as sequence ->
+                                    if sequence.Id |> System.String.IsNullOrEmpty then failwith "Sequence type in choice should have `id` declared"
+                                    acc.Add(ComplexType sequence.Id) |> ignore
+                                | _ -> failwith <| sprintf "Oh no: %O!" item
+                                acc)
+                            (HashSet<_>())
+        fieldTypes
+        |> Seq.iteri (fun i et ->
+            let ctr = CodeConstructor(Attributes=MemberAttributes.Private)
+            ctr.Parameters.Add(CodeParameterDeclarationExpression(CodeTypeReference("Tag"), "tag")) |> ignore
+            let name = sprintf "value%d" i
+            let tpRef = match et with
+                        | SimpleType tp -> CodeTypeReference(tp)
+                        | ComplexType name -> CodeTypeReference(name)
+            ctr.Parameters.Add(CodeParameterDeclarationExpression(tpRef, "value")) |> ignore
+            ctr.Statements.Add(CodeAssignStatement(CodeFieldReferenceExpression(CodeThisReferenceExpression(), "tag"), CodeVariableReferenceExpression("tag"))) |> ignore
+            ctr.Statements.Add(CodeAssignStatement(CodeFieldReferenceExpression(CodeThisReferenceExpression(), name), CodeVariableReferenceExpression("value"))) |> ignore
+            choiceType.Members.Add(ctr) |> ignore
+            choiceType.Members.Add(CodeMemberField(tpRef, name, Attributes=MemberAttributes.Private)) |> ignore)
+
+        let createNewMethod name (tpRef: CodeTypeReference) =
+            let createMethod = CodeMemberMethod(Name=(sprintf "New%s" name))
+            createMethod.ReturnType <- CodeTypeReference(choiceType.Name)
+            createMethod.Parameters.Add(CodeParameterDeclarationExpression(tpRef, "value")) |> ignore
+            createMethod.Attributes <- MemberAttributes.Static ||| MemberAttributes.FamilyAndAssembly
+            createMethod.Statements.Add(CodeMethodReturnStatement(CodeObjectCreateExpression(choiceType.Name, CodeFieldReferenceExpression(CodeTypeReferenceExpression("Tag"), name), CodeVariableReferenceExpression("value")))) |> ignore
+            createMethod
+
+        choiceTypeItems |> List.iter (fun item ->
             match item with
             | :? XmlSchemaElement as element ->
+                let tpRef = match matchType element.SchemaTypeName with
+                            | SimpleType tp -> CodeTypeReference(tp)
+                            | ComplexType name -> CodeTypeReference(name)
                 tagEnum.Members.Add(CodeMemberField("Tag", element.Name)) |> ignore
+                choiceType.Members.Add(createNewMethod element.Name tpRef) |> ignore
             | :? XmlSchemaSequence as sequence ->
-                if sequence.Id |> System.String.IsNullOrEmpty then
-                    failwith "Sequence type in choice should have `id` declared"
                 tagEnum.Members.Add(CodeMemberField("Tag", sequence.Id)) |> ignore
-                printfn "TODO: Sequence!!"
-            | _ -> failwith <| sprintf "Not implemented %O" item
-        choiceType
+                let sequenceType = CodeTypeDeclaration(sequence.Id, IsClass=true)
+                addedMembers.Add(sequenceType)
+                choiceType.Members.Add(createNewMethod sequence.Id (CodeTypeReference(sequence.Id))) |> ignore
+            | _ -> failwith <| sprintf "Not implemented %O" item)
+
+        let expReaderLocalName = CodePropertyReferenceExpression(varReader, "LocalName")
+
+        let choiceVarName = sprintf "v%d" !variableIndex
+        variableIndex := !variableIndex + 1
+        deserializeMethod.Statements.Add(CodeVariableDeclarationStatement(choiceType.Name, choiceVarName)) |> ignore
+        let rec buildDeserializeStatements (items: XmlSchemaObject list): CodeStatement =
+            match items with
+            | [] -> upcast CodeThrowExceptionStatement(CodeObjectCreateExpression(CodeTypeReference(typeof<System.Exception>), CodePrimitiveExpression("bla")))
+            | x::xs -> match x with
+                       | :? XmlSchemaElement as element ->
+                            let createChoice = CodeMethodInvokeExpression(CodeTypeReferenceExpression(choiceType.Name), sprintf "New%s" element.Name, CodeMethodInvokeExpression(varReader, "ReadString"))
+                            upcast CodeConditionStatement(CodeBinaryOperatorExpression(expReaderLocalName, CodeBinaryOperatorType.IdentityEquality, CodePrimitiveExpression(element.Name)),
+                                                          [| CodeAssignStatement(CodeVariableReferenceExpression(choiceVarName), createChoice) :> CodeStatement |],
+                                                          [| buildDeserializeStatements xs |])
+                       | :? XmlSchemaSequence as sequence ->
+                            let firstElement = sequence.Items.[0] :?> XmlSchemaElement
+                            upcast CodeConditionStatement(CodeBinaryOperatorExpression(expReaderLocalName, CodeBinaryOperatorType.IdentityEquality, CodePrimitiveExpression(firstElement.Name)),
+                                                          [| |],
+                                                          [| buildDeserializeStatements xs |])
+                       | _ -> failwith <| sprintf "Not implemented %O" x
+        deserializeMethod.Statements.Add([ for i in choiceElement.Items -> i] |> List.ofSeq |> buildDeserializeStatements) |> ignore
+        addedMembers
 
     match requestType.Particle with
     | :? XmlSchemaSequence as sequence ->
         for item in sequence.Items do
             match item with
-            | :? XmlSchemaChoice as choice ->
-                newMembers.Add(createChoiceType choice)
+            | :? XmlSchemaChoice as choice -> newMembers.AddRange(createChoiceType choice)
             | _ -> failwith <| sprintf "Not implemented %O" item
     | _ -> failwith <| sprintf "Not implemented %O" requestType.Particle
+
+    deserializeMethod.Statements.Add(CodeMethodReturnStatement(CodeArrayCreateExpression(typeof<obj>, [for i in 1..(!variableIndex - 1) -> CodeVariableReferenceExpression(sprintf "v%d" i) :> CodeExpression] |> List.toArray))) |> ignore
 
     newMembers
 
@@ -100,5 +181,6 @@ let BuildCodeUnit assemblyNamespace schemaFile =
 
     let codeCompileUnit = CodeCompileUnit()
     codeCompileUnit.Namespaces.Add(codeNamespace) |> ignore
+    codeCompileUnit.ReferencedAssemblies.Add("System.Xml.dll") |> ignore
 
     codeCompileUnit
--- XsdTool/Program.fs
+++ XsdTool/Program.fs
@@ -1,21 +1,36 @@
 ﻿module XsdTool.Program
 
-open FSharp.Configuration
 open Microsoft.CSharp
+open System.CodeDom
 open System.CodeDom.Compiler
 open System.IO
+open System.Text
 open XsdTool.CodeGenerator
 
-type Settings = AppSettings<"App.config">
+type Settings = FSharp.Configuration.AppSettings<"App.config">
+
+let printCode (codeUnits: CodeCompileUnit []) (codeProvider: #CodeDomProvider) =
+    codeUnits
+    |> Array.iter (fun u -> let sb = StringBuilder()
+                            use writer = new StringWriter(sb)
+                            codeProvider.GenerateCodeFromCompileUnit(u, writer, CodeGeneratorOptions())
+                            printfn "%s" (sb.ToString()))
+
+let compileAssembly (codeUnits: CodeCompileUnit []) (codeProvider: #CodeDomProvider) =
+    let parameters = CompilerParameters(GenerateExecutable=false, OutputAssembly=Settings.AssemblyName)
+    let results = codeProvider.CompileAssemblyFromDom(parameters, codeUnits)
+    [for str in results.Output -> str] |> Seq.iter (printfn "%s")
 
 [<EntryPoint>]
-let main argv =
+let main _ =
     let path = Path.GetFullPath(match Settings.XsdSearchPath with | null | "" -> "." | path -> path)
     let codeUnits = Directory.GetFiles(path, "*.xsd") |> Array.map (BuildCodeUnit Settings.AssemblyNamespace)
 
     use codeProvider = new CSharpCodeProvider()
-    let parameters = CompilerParameters(GenerateExecutable=false, OutputAssembly=Settings.AssemblyName)
-    let results = codeProvider.CompileAssemblyFromDom(parameters, codeUnits)
 
-    [for str in results.Output -> str] |> Seq.iter (printfn "%s")
+    let execute = printCode codeUnits
+    //let execute = compileAssembly codeUnits
+
+    codeProvider |> execute
+
     0
